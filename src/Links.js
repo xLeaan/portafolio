f98f09df2587ab28af60661cff40409045244776
Lean Pastor <leanandrespr@gmail.com>
2024-06-14 22:25:59 -0500

    portafolio

---
+++ src/Links.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './styles/Links.css'
+
+export const Links = () => {
+  return (
+    <div className='Links'>
+        <h6 className='title'>MENU</h6>
+        <div className='nav-links'>
+          <a>Experiencia.</a>
+          <br />
+          <a>Trabajos.</a>
+          <br />
+          <a>Contacto.</a>
+        </div>
+        
+        
+    </div>
+  )
+}
